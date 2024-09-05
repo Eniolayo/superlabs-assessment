8ccc0138685a6fb7b09e36cc1d62d398525e55b2
Eniolayo <eniolayodeji@gmail.com>
2024-09-05 10:35:10 +0100

    chore: fix deployment and add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,37 @@
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { ToastProvider } from "@/components/Toast/ToastContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ToastProvider>
-      <Component {...pageProps} />
-    </ToastProvider>
+    <>
+      <Head>
+        <title>Superlabs Assessment</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <ToastProvider>
+        <Component {...pageProps} />
+      </ToastProvider>
+    </>
   );
 }
